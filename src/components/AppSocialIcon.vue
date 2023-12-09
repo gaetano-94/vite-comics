86ca49ca72a9a1f337900b12ae82d5f3c24e0c39
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-09 13:15:18 +0100

    feat: footer completed

--- src/components/AppSocialIcon.vue
+++ src/components/AppSocialIcon.vue
@@ -11,7 +11,36 @@ export default {
         <a href="#">Sing-Up Now!</a>
       </h2>
     </div>
-    <div class="container-icon"></div>
+    <div class="container-icon">
+      <h2>Follow Us</h2>
+      <ul class="container-img">
+        <li>
+          <a href="#">
+            <img src="/img/footer-facebook.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="/img/footer-twitter.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="/img/footer-youtube.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="/img/footer-pinterest.png" alt="" />
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="/img/footer-periscope.png" alt="" />
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -35,6 +64,24 @@ export default {
       color: white;
       font-size: 20px;
     }
+  }
+
+  .container-icon {
+    display: flex;
+    gap: 20px;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+      color: $secondary;
+    }
+  }
+
+  .container-img {
+    list-style-type: none;
+    display: flex;
+    gap: 20px;
+    padding-right: 30px;
   }
 }
 </style>
