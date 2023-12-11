dda24da6642c666c5e66d516b7f87aeca92ad29e
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-11 15:53:59 +0100

    feat: jumbotron

---
+++ src/components/AppMainTop.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'MainTop',
+};
+</script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  height: 400px;
+  background-image: url(/img/jumbotron.jpg);
+  background-position: top;
+}
+</style>
